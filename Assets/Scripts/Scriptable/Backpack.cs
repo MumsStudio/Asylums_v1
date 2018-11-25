85c95ed0311c43d8c305f63c6a220d27539c941d
Liam <urehelonn@gmail.com>
2018-11-24 22:02:07 -0500

    backpack and item class build

---
+++ Assets/Scripts/Scriptable/Backpack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backpack : MonoBehaviour
+{
+    public List<Item> items;
+
+    //add item into backpack
+    public void addToBackpack(Item item)
+    {
+    }
+
+    public void removeFromBackpack(Item item)
+    {
+        
+    }
+}
