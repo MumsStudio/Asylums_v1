05fd5cf2f2fc1b53c2b5db6bdaefd131da4bfcf7
Liam <urehelonn@gmail.com>
2018-12-01 05:07:44 -0500

    Scene controller base added

---
+++ Assets/Scripts/SceneControl/Room.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewRoom")]
+public class Room : ScriptableObject {
+    public string roomId;
+    public GameObject roomObj;
+    public bool roomOn;
+}
---
+++ Assets/Scripts/SceneControl/SceneController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevel")]
+public class SceneController : ScriptableObject {
+    public string levelId;
+    public List<Room> roomsInLevel;
+}
