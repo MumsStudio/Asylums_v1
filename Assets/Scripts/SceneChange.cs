de415d5ec7d00d87cababac36578788d6e9fa827
madhotdog <jaymjj828@gmail.com>
2018-12-08 21:19:09 -0500

    add more UI for MainMenu and add sound effect for button click

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -20,4 +20,7 @@ public class SceneChange : MonoBehaviour {
         Debug.Log("Quit1!");
         Application.Quit();
     }
+
+
+
 }
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+
+	public void Onhover()
+    {
+        source.PlayOneShot(hover);
+    }
+
+    public void Onclick()
+    {
+        source.PlayOneShot(click);
+    }
+}
