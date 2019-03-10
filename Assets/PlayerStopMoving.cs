47fdd546e0e12372a8e82a0f8696b0d27a017913
JKJ <asd446902934@qq.com>
2019-03-09 20:50:56 -0400

    Menu fixed, dialogue fixed.
    Player now cannot move since menu is activated.
    Dialogue system words size fixed.

---
+++ Assets/PlayerStopMoving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStopMoving : MonoBehaviour {
+    private playercontroller PMove;
+    public void Pasue()
+    {
+        PMove = FindObjectOfType<playercontroller>();
+        PMove.canMove = false;
+    }
+    public void resume()
+    {
+        PMove.canMove = true;
+    }
+  
+}
