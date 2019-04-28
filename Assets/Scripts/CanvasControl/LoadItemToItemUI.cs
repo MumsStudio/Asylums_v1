b8c0a263e974d0ec0478a28e2feb122896b2648f
Unknown <urehelonn@gmail.com>
2019-04-28 02:27:47 -0400

    ui backpack text added, npc wooden added

---
+++ Assets/Scripts/CanvasControl/LoadItemToItemUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadItemToItemUI : MonoBehaviour {
+    public GameObject backpack;
+    public GameObject itemText;
+    	
+	// Update is called once per frame
+	public void UpdateItemTextBox () {
+        string listOfItem = "";
+        foreach(Item item in backpack.GetComponentInChildren<Backpack>().items)
+        {
+            listOfItem += " " + item.itemName;
+        }
+        itemText.GetComponentInChildren<Text>().text = listOfItem;
+	}
+}
