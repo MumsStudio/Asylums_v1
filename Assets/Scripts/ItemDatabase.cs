02fdcd1f7a64073656763e70d336e31e8c53856a
JKJ <asd446902934@qq.com>
2019-05-27 00:33:51 -0300

    Database created
    Database script created

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+    public List<DatabaseItem> ItemList;
+    //lists: Item IT,Infordb InD,Event ED.
+    //     ; IT.id, IT.information
+    //     ; search IT.id
+
+   // public const string itemInitial = "item";
+}
+[System.Serializable]
+public class  DatabaseItem : System.Object
+{
+    public int id;
+    public Item item;    
+}
+[System.Serializable]
+public class DatabaseInfordb : System.Object
+{
+    public DatabaseInfordb() { }
+    public int id;
+    public Info Infordb;
+}
+[System.Serializable]
+public class DatabaseEvent : System.Object
+{
+    public DatabaseEvent() { }
+    public int id;
+    public PlotEvent Event;
+}
