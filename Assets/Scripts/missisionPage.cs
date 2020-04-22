ec1c72d90c18c72c763180196a4d3d8520ae039b
madhotdog <jaymjj828@gmail.com>
2020-04-22 05:54:51 -0400

    add new page to display mission detail on mainmenu by jinjin

---
+++ Assets/Scripts/missisionPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class missisionPage : MonoBehaviour
+{
+    public Button mission1;
+    public Button mission2;
+    public Button mission3;
+    public Text content;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn1 = mission1.GetComponent<Button>();
+        btn1.onClick.AddListener(delegate (){ content.text = "Search 7 floor and find the way out"; });
+
+        
+    }
+
+    
+}
