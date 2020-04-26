bbb6a7c4fccd1156b60cede88de13a1eb2d524fa
Liam <urehelonn@gmail.com>
2020-04-25 22:48:28 -0400

    yibu
    timeline function improvement

---
+++ Assets/Scripts/Timeline/TimelineController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    private void OnTriggerEnter2D(Collider2D colli)
+    {
+        playableDirector.Play();
+    }
+}
