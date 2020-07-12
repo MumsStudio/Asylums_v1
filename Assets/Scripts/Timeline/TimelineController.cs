047dcdc49af71e6b8153ebdd74bccde84e390956
Liam <urehelonn@gmail.com>
2020-07-11 22:48:43 -0400

    timeline can trigger enforced event
    and some minor bug fixing

--- Assets/Scripts/Timeline/TimelineController.cs
+++ Assets/Scripts/Timeline/TimelineController.cs
@@ -6,9 +6,21 @@ using UnityEngine.Playables;
 public class TimelineController : MonoBehaviour
 {
     public PlayableDirector playableDirector;
+    public DialogHolder dialogHolder;
+    public int delaytime;
 
     private void OnTriggerEnter2D(Collider2D colli)
     {
         playableDirector.Play();
+        if (dialogHolder != null)
+        {
+            StartCoroutine(StartPlotDisplay());
+        }
+    }
+
+    private IEnumerator StartPlotDisplay()
+    {
+        yield return new WaitForSeconds(delaytime);
+        dialogHolder.enforcedEventCalled = true;
     }
 }
